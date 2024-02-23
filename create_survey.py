39d9d304812d1e3566ecd267d808647faaae2e3a
Zack Gottesman <zachary.s.gottesman.22@dartmouth.edu>
2024-02-23 01:31:50 -0500

    Create create_survey.py

---
+++ create_survey.py
@@ -0,0 +1,16 @@
+def create_survey_content():
+    pass    
+    
+def qualtrics_transer():
+    """
+    This function is responsible for preparing survey data for Qualtrics. 
+    It handles taking the data from the general format and massagin it into the Qualtrics format for submission to the api.
+    """
+    pass
+
+def select_questions():
+    """
+    This function is responsible for selecting the questions (pairs of texts) that will be used in a survey.
+    It will take into account the frequency of the previously used pairs.
+    """
+    pass
